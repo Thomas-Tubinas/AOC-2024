bce5da8f00cf8399f4b355e21f810645cc9d2df8
Phishing-Tides <143620957+Thomas-Tubinas@users.noreply.github.com>
2024-12-03 04:42:42 +0000

    errorcodes

--- Day1.py
+++ Day1.py
@@ -1,1 +1,25 @@
+list1 = []
+list2 = []
 
+def Load():
+    open with ("sortedList1.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            list1.appead(line)
+
+    open with ("sortedList2.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            list2.appead(line)
+
+finalDistance
+def Dicer():
+    for items in list1:
+        for i in range(5):
+            l1 += items[i]
+            l2 += list2[i]
+            print(l1 + l2)
+        //12345
+    
