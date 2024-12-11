396672882869db1e0ecf78734589bf2af0fbc1eb
Phishing-Tides <143620957+Thomas-Tubinas@users.noreply.github.com>
2024-12-11 00:30:50 +0000

    day 2 done

--- Day2.py
+++ Day2.py
@@ -15,7 +15,31 @@ def Checker():
         changes = [report[i + 1] - report[i] for i in range(len(report) - 1)]
         if all(1 <= diff <= 3 for diff in changes) or all(-3 <= diff <= -1 for diff in changes):
             safeReports += 1
+        else: 
+            AdvChecker(changes, report)
+
+advsafeReports = 0
+def AdvChecker(changes, report):
+    global advsafeReports
+    global list
+    for i in range(len(report)):
+        isSafe = False
+        advChanges = report.copy()
+        advChanges.pop(i)
+        reevaluation = [advChanges[i + 1] - advChanges[i] for i in range(len(advChanges) - 1)]
+        for i in range(len(report)):
+            if all(1 <= diff <= 3 for diff in reevaluation) or all(-3 <= diff <= -1 for diff in reevaluation):
+                print("good")
+                print(report)
+                print(changes)
+                print(advChanges)
+                advsafeReports += 1
+                isSafe = True
+                break
+        if(isSafe):
+            break
 
 Load()
 Checker()
 print(safeReports)
+print(advsafeReports)
